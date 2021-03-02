ca1936d68d418259fd0bceaf2a68f6598142e3db
dev <dev@devs.local>
2021-03-02 22:35:53 +0300

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,6 @@ str1++;
 while (s2[str2])
 str2++;
 
-if (s1 && s2)
 str = malloc(sizeof(str) * (str1 + str2));
 
 if (str == NULL)
@@ -33,7 +32,7 @@ return (NULL);
 for (i = 0; i < (str1 + str2); i++)
 str[i] = i < str1 ? s1[i] : s2[j++];
 
-*str = '\0';
+str = '\0';
 
 return (str);
 }
