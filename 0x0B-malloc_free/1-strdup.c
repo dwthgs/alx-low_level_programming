009c98b76d1bc1ea6b65987dd656202e464c2cd5
dev <dev@devs.local>
2021-03-02 14:13:48 +0300

    returns a pointer to a newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,11 +8,13 @@
 
 char *_strdup(char *str)
 {
-char *str_copy, *str_ptr;
+char *str_copy, *str_ptr, *temp;
 int i = 0;
 
 if (!str)
 return (NULL);
+
+temp = str;
 
 while (i < str[i])
 i++;
@@ -23,8 +25,8 @@ if (str_copy == NULL)
 return (NULL);
 
 str_ptr = str_copy;
-while (*str)
-*str_copy++ = *str++;
+while (*temp)
+*str_copy++ = *temp++;
 
 *str_copy = '\0';
 
