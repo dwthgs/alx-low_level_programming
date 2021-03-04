ad60a38dbeca074cd20872e861ad5f5c6e8e7ece
dev <dev@devs.local>
2021-03-04 22:45:04 +0300

    update

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +8,9 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-void free_grid(int **grid, int height);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
+
+#endif
