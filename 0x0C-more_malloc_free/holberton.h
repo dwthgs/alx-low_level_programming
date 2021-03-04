915d8f262de9bd3ad50f2798b66733124cb0fddd
dev <dev@devs.local>
2021-03-04 23:57:01 +0300

    update

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -8,5 +8,9 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _isdigit(int c);
+int _strlen(char *s);
+int _atoi(char *s);
+void errorexit(void);
+char *big_multiply(char *s1, char *s2);
 
 #endif /**HOLBERTON_H*/
