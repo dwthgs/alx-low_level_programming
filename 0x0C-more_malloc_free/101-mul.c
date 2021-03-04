8deb2ca1fd70610c912fea1054c994d55c75d5ce
dev <dev@devs.local>
2021-03-04 23:56:46 +0300

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,56 +1,116 @@
-
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
- * _isdigit  - Checks for a digit
- * @c: parameter c
- *
- * Description: Checks for a digit.
- * Betty Style
- * Return: Return 1 is a digit 0 otherwise
+ * _isdigit - checks for a digit
+ * @c: Digit
+ * Return: 1 if c is a digit, 0 otherwise
  */
+
 int _isdigit(int c)
 {
-if (c >= '0' && c <= '9')
-	return (1);
-else
-	return (0);
-
-return (0);
+return (c >= '0' && c <= '9');
 }
 
 /**
- * main - Entry
- *
- * @argc: count
- * @argv: vector.
- *
- * Return: multiply
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length of the string
  */
-int main(int argc, char *argv[])
+
+int _strlen(char *s)
 {
-int c, i = 0, sum = 0;
+int len = 0;
+
+while (s[len])
+len++;
+
+return (len);
+}
+
+/**
+ * mult - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
+ *
+ * Return: the product big number string
+ */
+char *mult(char *s1, char *s2)
+{
+char *r;
+int l1, l2, a, b, c, x;
+
+l1 = _strlen(s1);
+l2 = _strlen(s2);
+r = malloc(a = x = l1 + l2);
+if (!r)
+printf("Error\n"), exit(98);
+while (a--)
+r[a] = 0;
+
+for (l1--; l1 >= 0; l1--)
+{
+if (!_isdigit(s1[l1]))
+{
+free(r);
+printf("Error\n"), exit(98);
+}
+a = s1[l1] - '0';
+c = 0;
+
+for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
+{
+if (!_isdigit(s2[l2]))
+{
+free(r);
+printf("Error\n"), exit(98);
+}
+b = s2[l2] - '0';
+
+c += r[l1 + l2 + 1] + (a *b);
+r[l1 + l2 + 1] = c % 10;
+
+c /= 10;
+}
+if (c)
+r[l1 + l2 + 1] += c;
+}
+return (r);
+}
+
+
+/**
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
+ *
+ * Return: Always 0 on success.
+ */
+int main(int argc, char **argv)
+{
+char *r;
+int a, c, x;
+
 if (argc != 3)
+printf("Error\n"), exit(98);
+
+x = _strlen(argv[1]) + _strlen(argv[2]);
+r = mult(argv[1], argv[2]);
+c = 0;
+a = 0;
+while (c < x)
 {
-	printf("Error\n");
-	exit(98);
+if (r[c])
+a = 1;
+if (a)
+putchar(r[c] + '0');
+c++;
 }
-for (c = 1; c < argc; c++)
-{
-while (*(argv[c] + i) != '\0')
-{
-	if (!_isdigit(*(argv[c] + i)))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	i++;
-}
-sum = atoi(argv[1]) * atoi(argv[2]);
-}
-printf("%d\n", sum);
+if (!a)
+putchar('0');
+putchar('\n');
+free(r);
 return (0);
 }
