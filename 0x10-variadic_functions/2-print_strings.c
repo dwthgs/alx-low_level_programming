8c727f358ba6a469078d32a000c98436df0220cc
dev <dev@devs.local>
2021-03-11 23:30:48 +0300

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -8,22 +8,21 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-va_list valist;
+va_list ab;
 unsigned int i;
+char *s;
 
-if (separator == NULL)
-return;
-
-va_start(valist, n);
-
+va_start(ab, n);
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(valist, const unsigned int));
-
-if (i != (n - 1))
+s = va_arg(ab, char *);
+if (!s)
+printf("(nil)");
+if (s)
+printf("%s", s);
+if (separator && i < n - 1)
 printf("%s", separator);
 }
 printf("\n");
-
-va_end(valist);
+va_end(ab);
 }
